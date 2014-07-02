3a02d9546d5b7a901d6ffeb27d49c2f7450ff7a5
Henry Huang <Henry.Huang@EF.com>
2014-07-02 07:53:56 -0400

    init and begin

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ require(["jquery", "three"],
     console.log("Three");
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
   }
 );
